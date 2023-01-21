dd29753aae4a3a8030af53c35a0800abcb08fd7a
Aprea <apreak@gmail.com>
2023-01-21 10:27:04 +0700

    checkpoint email

--- simklinik/bidan/views.py
+++ simklinik/bidan/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -6,6 +7,11 @@ from django.core.mail import EmailMessage
 from django.conf import settings
 from django.contrib import messages
 from .models import Pendaftaran
+from django.views.generic import ListView
+from django.template import Context
+from django.template.loader import render_to_string, get_template
+
+
 # Create your views here.
 
 
@@ -29,7 +35,6 @@ class HomeTemplateView(TemplateView):
         messages.add_message(request, messages.SUCCESS,
                              f"Terimakasih {name} telah menghubungi kami, email anda berhasil terkirim..")
         return HttpResponseRedirect(request.path)
-        # return HttpResponse('Email telah sukses terkirim, silahkan tunggu admin kami akan segera membalas pesan anda...')
 
 
 class PendaftaranTemplateView(TemplateView):
@@ -57,31 +62,45 @@ class PendaftaranTemplateView(TemplateView):
         return HttpResponseRedirect(request.path)
 
 
-class ManagePendaftaranTemplateView(TemplateView):
+class ManagePendaftaranTemplateView(ListView):
     template_name = 'listpendaftar.html'
+    model = Pendaftaran
+    context_object_name = "pendaftaran"
     login_required = True
+    paginate_by = 3
+
+    def post(self, request):
+        date = request.POST.get("date")
+        id_pendaftaran = request.POST.get("pendaftaran-id")
+
+        pendaftaran = Pendaftaran.objects.get(
+            id=id_pendaftaran,
+        )
+        pendaftaran.daftar_konfirmasi = True
+        pendaftaran.tanggal_konfirmasi = datetime.datetime.now()
+        pendaftaran.save()
+
+        data = {
+            "name": pendaftaran.name,
+            "date": date
+        }
+        message = get_template('email.html').render(data)
+        email = EmailMessage(
+            "Pendaftaran di Klinik Bidan",
+            message,
+            settings.EMAIL_HOST_USER,
+            [pendaftaran.email]
+        )
+        email.content_subtype = "html"
+        email.send()
+
+        messages.add_message(request, messages.SUCCESS, f"{date}")
+        return HttpResponseRedirect(request.path)
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         pendaftaran = Pendaftaran.objects.all()
         context.update({
-            "pendaftaran": pendaftaran,
             "title": "Manage Pendaftaran"
         })
         return context
-
-
-# class ManagePendaftaranTemplateView(ListView):
-#     template_name = 'listpendaftar.html'
-#     model = Pendaftaran
-#     context_object_name = "pendaftaran"
-#     login_required = True
-#     paginate_by = 5
-
-#     def get_context_data(self, *args, **kwargs):
-#         context = super().get_context_data(*args, **kwargs)
-#         pendaftaran = Pendaftaran.objects.all()
-#         context.update({
-#             "title": "Manage Pendaftaran"
-#         })
-#         return context
