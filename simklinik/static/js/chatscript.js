9c5dc8d1b1dc24a85b50f238c50bc8f960484393
Aprea <apreak@gmail.com>
2023-07-07 10:55:43 +0700

    update chatbot

--- simklinik/static/js/chatscript.js
+++ simklinik/static/js/chatscript.js
@@ -15,9 +15,25 @@
 // })(document, window.kommunicate || {});
 // /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
 
+// (function (d, m) {
+//   var kommunicateSettings = {
+//     appId: "2233597bd4fd6b10c7138d94909675c25",
+//     popupWidget: true,
+//     automaticChatOpenOnNavigation: true,
+//   };
+//   var s = document.createElement("script");
+//   s.type = "text/javascript";
+//   s.async = true;
+//   s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+//   var h = document.getElementsByTagName("head")[0];
+//   h.appendChild(s);
+//   window.kommunicate = m;
+//   m._globals = kommunicateSettings;
+// })(document, window.kommunicate || {});
+
 (function (d, m) {
   var kommunicateSettings = {
-    appId: "2233597bd4fd6b10c7138d94909675c25",
+    appId: "376d106badab454f9b56fedbe248b6012",
     popupWidget: true,
     automaticChatOpenOnNavigation: true,
   };
